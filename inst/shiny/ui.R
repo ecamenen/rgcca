ad4958963d45b3f705d52af2ee60d85e7bdf51d7
Etienne Camenen <etienne.camenen@gmail.com>
2019-07-26 15:13:37 +0200

    removed app.R

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -9,12 +9,23 @@
 # the samples and the variables projected on the two first component of the multi-block analysis, the histograms
 # of the most explicative variables and the explained variance for each blocks.
 
+rm(list=ls())
+options(shiny.maxRequestSize = 30*1024^2)
+
 setInfo <- function(., text){
   shinyInput_label_embed(
     icon("question") %>%
       bs_embed_tooltip(title = text)
   )
 }
+
+one_block <<- c(`Principal Component Analysis` = "PCA")
+two_blocks <<- c(`Canonical Correlation Analysis` = 'CCA', `Interbattery Factor Analysis` = "IFA", `Partial Least Squares Regression` = 'PLS',  `Redundancy analysis` = 'RA')
+multiple_blocks  <<- c(`Regularized Generalized CCA (RGCCA)` = 'RGCCA', `Sparse Generalized CCA (SGCCA)` = 'SGCCA', `SUM of CORrelations method` = 'SUMCOR', `Sum of SQuared CORrelations method` = 'SSQCOR',
+                      `Sum of ABSolute value CORrelations method` = 'SABSCOR',`SUM of COVariances method` = 'SUMCOV',`Sum of SQuared COVariances method` = 'SSQCOV',
+                      `Sum of ABSolute value COVariances method` = 'SABSCOV', `MAXBET` = 'MAXBET', `MAXBETB` = 'MAXBET-B')
+multiple_blocks_super  <<- c(`Generalized CCA (GCCA)` = 'GCCA', `Hierarchical PCA` = 'HPCA', `Multiple Factor Analysis` = 'MFA')
+analyse_methods  <<- list(one_block, two_blocks, multiple_blocks, multiple_blocks_super)
 
 # Libraries loading
 librairies = c("RGCCA", "ggplot2", "scales", "plotly", "visNetwork", "devtools", "igraph", "shiny", "shinyjs", "bsplus")
